2eec1c71084b8be468262c0cdf161b540e57d4de
Emmagitlab <emma2014yang@hotmail.com>
2016-09-26 16:15:06 -0400

    Update MergeInterval.java

--- MergeInterval.java
+++ MergeInterval.java
@@ -13,11 +13,38 @@ return [1,6],[8,10],[15,18].*/
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
 public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
-        
+        if(intervals == null || intervals.size() <=1) return intervals;
+        Collections.sort(intervals, new Comparator<Interval>(){
+            public int compare(Interval i1, Interval i2){
+                return i1.start - i2.start;
+            }
+        });
+        Interval pre = intervals.get(0);
+        for(int i = 1; i < intervals.size();i++){
+            Interval curr = intervals.get(i);
+            if(pre.end >=curr.start){
+                pre.end = Math.max(curr.end,pre.end);
+                intervals.remove(curr);
+                i--;
+            }else{
+                pre = curr;
+            }
+        }
+        return intervals;
     }
 }
+
 
 
 

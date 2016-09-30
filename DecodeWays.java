a164708bb7de66a9f874cfa6a7706334825dfe67
Emmagitlab <emma2014yang@hotmail.com>
2016-09-30 01:50:47 -0400

    Update DecodeWays.java

--- DecodeWays.java
+++ DecodeWays.java
@@ -139,7 +139,7 @@ public class Solution {
     }
 }
 
-//mine recursive
+//mine recursive time complexity O(n)  space complexityO(n)
 
 public int numDecodings(String s) {
         if(s == null || s.length() == 0 || s == "0") return 0;
@@ -162,3 +162,27 @@ public int numDecodings(String s) {
         int num = Integer.parseInt(s);
         return num >=1 && num <= 26;
     }
+    
+// time O(n) space O(1)
+    public int numDecodings(String s){
+        if(s.length()==0) return 0;
+        int[] dp = new int[2];
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=0;i<s.length();i++){
+            int temp=0;
+            if(s.charAt(i)!='0')
+                temp+=dp[1];
+            if(i>0){
+                int a = Integer.parseInt(s.substring(i-1,i+1));
+                if(s.charAt(i-1)!='0' && a<=26 && a>=1)
+                    temp+=dp[0];
+            }
+            dp[0]=dp[1];
+            dp[1]=temp;
+        }
+        
+        return dp[1];
+    }
+

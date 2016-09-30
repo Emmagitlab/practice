07b4e9e3612997cd369e8fa35eb02c63ebd0e111
Emmagitlab <emma2014yang@hotmail.com>
2016-09-30 01:14:04 -0400

    Update ReadNUsingRead4MultipleTimes.java

--- ReadNUsingRead4MultipleTimes.java
+++ ReadNUsingRead4MultipleTimes.java
@@ -118,3 +118,24 @@ public class Solution extends Reader4 {
         return readBytes;
     }
 }
+
+
+    private int buffPtr = 0;
+    private int buffCnt = 0;
+    private char[] buff = new char[4];
+    public int read(char[] buf, int n) {
+        int ptr = 0;
+        while (ptr < n) {
+            if (buffPtr == 0) {
+                buffCnt = read4(buff);
+            }
+            if (buffCnt == 0) break;
+            while (ptr < n && buffPtr < buffCnt) {
+                buf[ptr++] = buff[buffPtr++];
+            }
+            if (buffPtr >= buffCnt) buffPtr = 0;
+        }
+        return ptr;
+    }
+I used buffer pointer (buffPtr) and buffer Counter (buffCnt) to store the data received in previous calls. 
+In the while loop, if buffPtr reaches current buffCnt, it will be set as zero to be ready to read new data.

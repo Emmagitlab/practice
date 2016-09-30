2c7a4a47288f4b6c37adedd0475a323f7d78a914
Emmagitlab <emma2014yang@hotmail.com>
2016-09-30 13:42:48 -0400

    Update BestTimeToBuyAndSellStock2.java

--- BestTimeToBuyAndSellStock2.java
+++ BestTimeToBuyAndSellStock2.java
@@ -35,3 +35,19 @@ public class Solution {
         return profit;
     }
 }
+
+
+// My code
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        for(int i = 1; i < prices.length; i++){
+            int diff = prices[i] - prices[i-1];
+            if(diff > 0){
+                result += diff;
+            }
+        }
+        return result;
+    }
+}

2a513c331dbed110e5fe2705231a3d09e08da0bd
Emmagitlab <emma2014yang@hotmail.com>
2016-10-09 16:25:30 -0400

    Update AddOneWithoutPlusOperator.java

--- AddOneWithoutPlusOperator.java
+++ AddOneWithoutPlusOperator.java
@@ -1,3 +1,10 @@
+
+
+
+//Given a non-negative number represented as an array of digits, plus one to the number.
+
+//The digits are stored such that the most significant digit is at the head of the list.
+
 public class Solution {
 	public void addOne(int num) {
 		//System.out.println(addOneHelper(num, 1));

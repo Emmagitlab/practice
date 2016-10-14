e69db8b3f2cb934a3f137440e52e5f373b89b2ad
Emmagitlab <emma2014yang@hotmail.com>
2016-10-14 13:54:18 -0400

    Update 3Sum4SumHashTable.java

--- 3Sum4SumHashTable.java
+++ 3Sum4SumHashTable.java
@@ -34,9 +34,35 @@ public class Solution {
 }
 
 
-2. 3sum:
-Answer:
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author YangE
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] num = {2, 4,2,1, 11, 15};
+        int target = 9;
+        List<List<Integer>> result = sol.fourSum(num, target);
+        sol.threeSum(num);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i).toString());
+        }
+
+    }
+
     public List<List<Integer>> threeSum(int[] num) {
         int sz = num.length;
 
@@ -44,33 +70,78 @@ public class Solution {
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
         List<Integer> tri = new ArrayList<Integer>();
 
-        for(int i=0; i<sz; ++i) {
+        for (int i = 0; i < sz; ++i) {
             int val = num[i];
-            if(m.containsKey(val)) {
+            if (m.containsKey(val)) {
                 List<Integer> l = m.get(val);
                 l.add(i);
-            } 
-            else {
+            } else {
                 ArrayList<Integer> l = new ArrayList<Integer>();
                 l.add(i);
                 m.put(val, l);
             }
         }
 
-
-        for(int i=0; i<sz; ++i) {
-            for(int j=i+1; j<sz; ++j) {
+        for (int i = 0; i < sz; ++i) {
+            for (int j = i + 1; j < sz; ++j) {
                 int o = 0 - num[i] - num[j];
-                if(m.containsKey(o)) {
+                if (m.containsKey(o)) {
                     List<Integer> l = m.get(o);
-                    for(int idx : l) {
-                        if(idx > j){
+                    for (int idx : l) {
+                        if (idx > j) {
                             tri.add(num[i]);
                             tri.add(num[j]);
                             tri.add(num[idx]);
                             if (ret.isEmpty() || !tri.equals(ret.get(ret.size() - 1))) {
                                 ret.add(tri);
-       }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    public List<List<Integer>> fourSum(int[] num, int target) {
+
+        HashMap<Integer, List<Integer>> hmap = new HashMap<Integer, List<Integer>>();
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        List<Integer> tup = new ArrayList<Integer>();
+        int sz = num.length;
+
+        for (int i = 0; i < sz; ++i) {
+            for (int j = i + 1; j < sz; ++j) {
+                //key: val, value: idx=i*sz+j;
+                int val = num[i] + num[j];
+                int idx = i * sz + j;                              // pair(i,j) <-> idx= i*sz +j;
+
+                if (hmap.containsKey(val)) {
+                    hmap.get(val).add(idx);
+                } else {
+                    List<Integer> l = new ArrayList<Integer>();
+                    l.add(idx);
+                    hmap.put(val, l);
+                }
+            }
+        }
+
+        for (int i = 0; i < sz; ++i) {
+            for (int j = i + 1; j < sz; ++j) {
+                int val = num[i] + num[j];
+                int o = target - val;
+
+                if (hmap.containsKey(o)) {
+                    List<Integer> l = hmap.get(o);
+                    for (int pos : l) {
+                        if (j < pos / sz) {                             //!important, to avoid duplicate!     
+                            tup.add(num[i]);
+                            tup.add(num[j]);
+                            tup.add(num[pos / sz]);
+                            tup.add(num[pos % sz]);
+                            if (ret.isEmpty() || !tup.equals(ret.get(ret.size() - 1))) {
+                                ret.add(tup);
+                            }
                         }
                     }
                 }
@@ -80,51 +151,3 @@ public class Solution {
     }
 }
 
-3.  4Sum:
-Answer:
-static List<List<Integer>> fourSum(int[] num, int target) {
-
-  HashMap<Integer, List<Integer>> hmap = new HashMap<Integer, List<Integer>>();
-                List<List<Integer>> ret = new ArrayList<List<Integer>>();
-                List<Integer> tup = new ArrayList<Integer>();
-  int sz = num.length;
-
-  for (int i = 0; i < sz; ++i) {
-   for (int j = i + 1; j < sz; ++j) {
-//key: val, value: idx=i*sz+j;
-    int val = num[i] + num[j];
-    int idx = i * sz + j;                              // pair(i,j) <-> idx= i*sz +j;
-    
-                                if (hmap.containsKey(val)) {
-     hmap.get(val).add(idx);
-    } else {
-     List<Integer> l = new ArrayList<Integer>();
-     l.add(idx);
-     hmap.put(val, l);
-    }
-   }
-  }
-
-  for (int i = 0; i < sz; ++i) {
-   for (int j = i + 1; j < sz; ++j) {
-    int val = num[i] + num[j];
-    int o = target - val;
-    
-    if (hmap.containsKey(o)) {
-     List<Integer> l = hmap.get(o);
-     for (int pos : l) {
-         if (j < pos / sz) {                             //!important, to avoid duplicate!     
-      tup.add(num[i]);
-      tup.add(num[j]);
-      tup.add(num[pos / sz]);
-      tup.add(num[pos % sz]);
-      if (ret.isEmpty() || !tup.equals(ret.get(ret.size() - 1))) {
-       ret.add(tup);
-          }
-     }
-    }
-   }
-  }
-  return ret;
- }
-}

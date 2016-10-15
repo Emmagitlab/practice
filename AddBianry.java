7469c07bb96637eff953eab12b3331e89b0516a8
Emmagitlab <emma2014yang@hotmail.com>
2016-10-15 15:54:11 -0400

    Update AddBianry.java

--- AddBianry.java
+++ AddBianry.java
@@ -6,6 +6,11 @@ b = "1"
 Return "100".*/
 
 
+
+import java.io.*;
+import java.util.*;
+
+
 public class Solution {
     public static void main(String[] args){
       Solution sol = new Solution();

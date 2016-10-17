8f6b75cb756a85db268296b4a19d194028db6795
Emmagitlab <emma2014yang@hotmail.com>
2016-10-17 11:19:12 -0400

    Update MeetingRooms2.java

--- MeetingRooms2.java
+++ MeetingRooms2.java
@@ -43,7 +43,13 @@ private static List<Integer> findMaxOverlap(int[] enter, int[] exit) {
   
  // Emma
  
-
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
   public class Solution {
     public int minMeetingRooms(Interval[] intervals) {
         int[] starts = new int[intervals.length];

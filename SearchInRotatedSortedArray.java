8d6058e25e58a3191595a54eb5fe503f0738b44c
Emmagitlab <emma2014yang@hotmail.com>
2016-10-17 10:42:30 -0400

    Update SearchInRotatedSortedArray.java

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -52,43 +52,64 @@ public class Solution {
 }
 
 
-// Float Value??
+//Java Solution 1- Recusive
 
-public class Solution {
-    public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-        	return -1;
-        }
-        int start = 0;
-        int end =  nums.length - 1;
-        int mid = start + (end - start) / 2;
-        while (start + 1 < end) {
-        	mid = start + (end - start) / 2;
-        	if (nums[mid] == target) {
-                return mid;
-            } 
-            if (nums[start] > nums[mid]) {
-                if (nums[mid] <= target && target <= nums[end]) {
-                    start = mid;
-                } else {
-                    end = mid;
-                }
-            } else {
-                if (nums[start] <= target && target <= nums[mid]) {
-                    end = mid;
-                } else {
-                    start = mid;
-                }
-            }
-        }
-        if (nums[start] == target) {
-        	return start;
-        }
-        if (nums[end] == target) {
-        	return end;
-        }
+public int search(int[] nums, int target) {
+    return binarySearch(nums, 0, nums.length-1, target);
+}
+ 
+public int binarySearch(int[] nums, int left, int right, int target){
+    if(left>right) 
         return -1;
+ 
+    int mid = left + (right-left)/2;
+ 
+    if(target == nums[mid])
+        return mid;
+ 
+    if(nums[left] <= nums[mid]){
+        if(nums[left]<=target && target<nums[mid]){
+          return binarySearch(nums,left, mid-1, target);
+        }else{
+          return  binarySearch(nums, mid+1, right, target);
+        }
+    }else {
+        if(nums[mid]<target&& target<=nums[right]){
+          return  binarySearch(nums,mid+1, right, target);
+        }else{
+          return  binarySearch(nums, left, mid-1, target);
+        }
     }
+}
+
+
+//Java Solution 2 - Iterative
+
+public int search(int[] nums, int target) {
+    int left = 0;
+    int right= nums.length-1;
+ 
+    while(left<=right){
+        int mid = left + (right-left)/2;
+        if(target==nums[mid])
+            return mid;
+ 
+        if(nums[left]<=nums[mid]){
+            if(nums[left]<=target&& target<nums[mid]){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }else{
+            if(nums[mid]<target&& target<=nums[right]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }    
+    }
+ 
+    return -1;
 }
 
 

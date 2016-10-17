80ff26154d0f3c1bc8a4016585cd5ff6053d65a9
Emmagitlab <emma2014yang@hotmail.com>
2016-10-17 11:36:24 -0400

    Update Strstr.java

--- Strstr.java
+++ Strstr.java
@@ -57,3 +57,23 @@ public class Solution {
         return -1;
     }
 }
+
+
+//Emma
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        int start = 0;
+        // 如果剩下的字母不够needle长度就停止遍历
+        while(start <= haystack.length() - needle.length()){
+            int i1 = start, i2 = 0;
+            while(i2 < needle.length() && haystack.charAt(i1)==needle.charAt(i2)){
+                i1++;
+                i2++;
+            }
+            if(i2 == needle.length()) return start;
+            start++;
+        }
+        return -1;
+    }
+}

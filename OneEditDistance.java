d164995498cfd3af28b480440851a73c5a232ff5
Emmagitlab <emma2014yang@hotmail.com>
2016-10-25 12:59:51 -0400

    Update OneEditDistance.java

--- OneEditDistance.java
+++ OneEditDistance.java
@@ -291,3 +291,23 @@ public boolean isOneModify(String s,String t){
     }
     return diff==1;
 }
+
+
+///EMMA
+
+public class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        int len = Math.min(s.length(),t.length());
+        for(int i = 0; i < len; i++){
+            if(s.charAt(i) != t.charAt(i)){
+                if(s.length() == t.length()){
+                    return s.substring(i+1).equals(t.substring(i+1));
+                } else if(s.length() < t.length()){
+                    return s.substring(i).equals(t.substring(i+1));
+                } else 
+                    return t.substring(i).equals(s.substring(i+1));
+            }
+        }
+        return Math.abs(s.length() - t.length()) == 1; 
+    }
+}

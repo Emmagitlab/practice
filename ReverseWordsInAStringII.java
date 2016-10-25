b2d7f3ce31a532f79b73d2195b8666357618a7b2
Emmagitlab <emma2014yang@hotmail.com>
2016-10-25 11:35:54 -0400

    Update ReverseWordsInAStringII.java

--- ReverseWordsInAStringII.java
+++ ReverseWordsInAStringII.java
@@ -11,28 +11,32 @@ return "blue is sky the".
 Could you do it in-place without allocating extra space?
 
 
-public class Solution {
 public void reverseWords(char[] s) {
-    int i=0;
-    for(int j=0; j<s.length; j++){
-        if(s[j]==' '){
-            reverse(s, i, j-1);        
-            i=j+1;
+public class Solution {
+   public void reverseWords(char[] s) {
+    // Three step to reverse
+    // 1, reverse the whole sentence
+    reverse(s, 0, s.length - 1);
+    // 2, reverse each word
+    int start = 0;
+    int end = -1;
+    for (int i = 0; i < s.length; i++) {
+        if (s[i] == ' ') {
+            reverse(s, start, i - 1);
+            start = i + 1;
         }
     }
- 
-    reverse(s, i, s.length-1);
- 
-    reverse(s, 0, s.length-1);
+    // 3, reverse the last word, if there is only one word this will solve the corner case
+    reverse(s, start, s.length - 1);
 }
- 
-public void reverse(char[] s, int i, int j){
-    while(i<j){
-        char temp = s[i];
-        s[i]=s[j];
-        s[j]=temp;
-        i++;
-        j--;
+
+public void reverse(char[] s, int start, int end) {
+    while (start < end) {
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
     }
 }
 }

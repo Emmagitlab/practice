a7916f9f088fabdaa2fb65ae6f1f4ebeea0e131c
Emmagitlab <emma2014yang@hotmail.com>
2016-11-15 19:03:19 -0500

    Update AddAndSearchWord.java

--- AddAndSearchWord.java
+++ AddAndSearchWord.java
@@ -17,7 +17,10 @@ search("b..") -> true
 Note:
 You may assume that all words are consist of lowercase letters a-z.*/
 
-insert and search costs O(key_length), however the memory requirements of trie is O(ALPHABET_SIZE * key_length * N) where N is number of keys in trie. There are efficient representation of trie nodes (e.g. compressed trie, ternary search tree, etc.) to minimize memory requirements of trie.
+insert and search costs O(key_length), however the memory requirements of trie 
+is O(ALPHABET_SIZE * key_length * N) where N is number of keys in trie. There are efficient
+representation of trie nodes (e.g. compressed trie, ternary search tree, etc.) to minimize memory 
+requirements of trie.
 
 class Node { 
 Node getChildForLetter(letter) 

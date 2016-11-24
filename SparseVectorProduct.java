f0a2a8a35a634bc7ace7586f68885fc3f9aa3a4f
Emmagitlab <emma2014yang@hotmail.com>
2016-11-24 03:10:45 -0500

    Update SparseVectorProduct.java

--- SparseVectorProduct.java
+++ SparseVectorProduct.java
@@ -31,10 +31,19 @@ public int[][] multiply(int[][] A, int[][] B) {
 }
 
 
-/*小哥说既然你有machine learning经验，那来你应该知道很多特征vector/matrix都是稀疏的，这题我见过啊！！！感谢地里！！先问我sparse vector 怎么表示比较好，然后让写代码求两个sparse vector dot product。再follow up， 如果一个vector 比另一个大很多怎么办，答对于小的vector里每一个（index， value），在大的里binary search。然后问了复杂度。
+/*小哥说既然你有machine learning经验，那来你应该知道很多特征vector/matrix都是稀疏的，
+这题我见过啊！！！感谢地里！！先问我sparse vector 怎么表示比较好，
+然后让写代码求两个sparse vector dot product。再follow up， 如果一个vector 比另一个大很多怎么办，
+答对于小的vector里每一个（index， value），在大的里binary search。然后问了复杂度。
 */
 
-一开始说连个hashmap，小哥说hashmap会浪费掉多余空间，我说那如果一个特别大的话就扫小一点的那个array，然后在特别大的array中用binary search，他说写代码。写完代码接着说，那如果差不多大，我说那就两个指针按照merge sort那么扫。然后我觉得基本都行了，他最后说那有没有O(Math.min(m, n))的方法。我鼓捣半天，最后说了个那就输入直接是一个tuple，第一个elem是位置（这个位置在两个array中必须都不是0），然后扫一遍就行了。其实我感觉他的意思是再用HashMap。不过他忘了之前和我说太浪费空间了。。。
+一开始说连个hashmap，小哥说hashmap会浪费掉多余空间，
+我说那如果一个特别大的话就扫小一点的那个array，
+然后在特别大的array中用binary search，他说写代码。
+写完代码接着说，那如果差不多大，我说那就两个指针按照merge sort那么扫。
+然后我觉得基本都行了，他最后说那有没有O(Math.min(m, n))的方法。我鼓捣半天，
+最后说了个那就输入直接是一个tuple，第一个elem是位置（这个位置在两个array中必须都不是0），
+然后扫一遍就行了。其实我感觉他的意思是再用HashMap。不过他忘了之前和我说太浪费空间了。。。
 
 /*于是第一个优化的想法是将向量变为这样的模式
 向量A:{<x1',loc1'>,<x2',loc2'>...<xn',locn'>}
@@ -49,9 +58,13 @@ public int[][] multiply(int[][] A, int[][] B) {
 这样计算代价为mlogn + min(m,n),前部为查找代价，后部为加法代价，加法代
 价必然比min(m,n)还要小,最大情况下为min(m,n)-1。*/
 
-是要保存两个arraylist 一个是存index，一个存value吗？为什么这个比较省空间呢？用hashmap 存index，value，比两个arraylist的性能差在哪里？
-第一题我是用的arraylist来存的，比较省空间，然后能保持index有序性，hashtable存的话follow up就不能二分法了吧。
-因为你只保存了非0元素呀，vector是稀疏的，这样就只需要保存很少。 hashmap存的话算dot product是没问题的，但Follow up里(index, value)表示后的向量一个很长，一个很短的话，就不能用二分查找了。我觉得是这样
+是要保存两个arraylist 一个是存index，一个存value吗？为什么这个比较省空间呢？
+用hashmap 存index，value，比两个arraylist的性能差在哪里？
+第一题我是用的arraylist来存的，比较省空间，然后能保持index有序性，
+hashtable存的话follow up就不能二分法了吧。
+因为你只保存了非0元素呀，vector是稀疏的，这样就只需要保存很少。 
+hashmap存的话算dot product是没问题的，但Follow up里(index, value)表示后的向量一个很长，
+一个很短的话，就不能用二分查找了。我觉得是这样
 
 int start = 0, end = nums.length - 1;
         while (start + 1 < end) {
@@ -123,9 +136,11 @@ index是sorted,所以两个List一起扫就出来出来了
 
 稀疏向量的存储方法是存储一个列表，里面存非零元素的位置和值，也就是<index, value>，按index排序.
 
-两个稀疏向量做点积的时候需要对index做归并，碰到有index相等的时候就把对应的两个value相乘，然后加到结果当中。归并完了就可以啦~
+两个稀疏向量做点积的时候需要对index做归并，碰到有index相等的时候就把对应的两个value相乘，
+然后加到结果当中。归并完了就可以啦~
 
-另外一个做法是用HashMap<index, value>，这样的话求点积就是遍历其中一个HashMap，在另外一个里面查找对应的index是否存在，这样也是可以的。
+另外一个做法是用HashMap<index, value>，这样的话求点积就是遍历其中一个HashMap，
+在另外一个里面查找对应的index是否存在，这样也是可以的。
 
 
 
